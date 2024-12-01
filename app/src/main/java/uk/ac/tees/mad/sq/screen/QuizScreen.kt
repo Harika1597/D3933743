118a6d2a539e2a899e224306a61bbf0fb553f540
Harika <harikathatipelli@gmail.com>
2024-12-01 19:45:55 +0000

    Quiz Screen

--- app/src/main/java/uk/ac/tees/mad/sq/screen/QuizScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/sq/screen/QuizScreen.kt
@@ -1,8 +1,10 @@
 package uk.ac.tees.mad.sq.screen
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,10 +19,14 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.KeyboardArrowLeft
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,8 +40,10 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import uk.ac.tees.mad.sq.R
 import uk.ac.tees.mad.sq.data.remote.Result
+import uk.ac.tees.mad.sq.ui.theme.permanentMarker
 import uk.ac.tees.mad.sq.ui.theme.poppins
 import uk.ac.tees.mad.sq.viewmodel.QuizViewModel
+import kotlin.random.Random
 
 @Composable
 fun QuizScreen(navController: NavHostController, viewModel: QuizViewModel, quizData: Result) {
@@ -43,6 +51,10 @@ fun QuizScreen(navController: NavHostController, viewModel: QuizViewModel, quizD
         colorScheme.primary,
         colorScheme.tertiary
     )
+    val selectedOption = rememberSaveable {
+        mutableStateOf("")
+    }
+    val shuffledAnswers = remember { getShuffledAnswers(quizData) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -84,15 +96,88 @@ fun QuizScreen(navController: NavHostController, viewModel: QuizViewModel, quizD
                     colorScheme.tertiary
                 )
         ) {
-            Box(modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp)
-                .padding(10.dp)
-                .clip(RoundedCornerShape(20.dp))){
-                Image(painter = painterResource(id = R.drawable.wireframe), contentDescription = null, modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop)
-                Text(text = quizData.question, fontFamily = poppins, fontSize = 22.sp, fontWeight = FontWeight.Bold, color = Color.White, modifier = Modifier.align(Alignment.Center))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .padding(10.dp)
+                    .clip(RoundedCornerShape(20.dp))
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.wireframe),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = quizData.question,
+                    fontFamily = poppins,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .padding(10.dp)
+                )
+            }
+            Column(modifier = Modifier.padding(20.dp)) {
+                for (answer in shuffledAnswers) {
+                    optionChoose(
+                        option = answer,
+                        selectedOption = selectedOption.value,
+                        onOptionClick = {
+                            selectedOption.value = answer
+                            Log.d("selectedOption", selectedOption.value)
+                        }
+                    )
+                }
             }
         }
     }
+}
+
+@Composable
+fun optionChoose(option: String, selectedOption: String, onOptionClick: () -> Unit) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(50.dp)
+        .padding(4.dp)
+        .border(1.dp,if (option == selectedOption) Color.Green else Color.LightGray, RoundedCornerShape(20.dp))
+        .clip(RoundedCornerShape(20.dp))
+        .background(if (option == selectedOption) Color(0xFF90EE90) else Color.White)
+        .clickable {
+            onOptionClick()
+        }) {
+        Row(modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(20.dp))) {
+            Text(
+                text = option,
+                fontFamily = permanentMarker,
+                color = Color.Black,
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .padding(horizontal = 20.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            if (option == selectedOption) {
+                Image(
+                    painter = painterResource(id = R.drawable.checked),
+                    contentDescription = "checked",
+                    modifier = Modifier
+                        .padding(end = 10.dp)
+                        .size(30.dp)
+                        .align(Alignment.CenterVertically)
+
+                )
+            }
+        }
+    }
+}
+
+fun getShuffledAnswers(result: Result): List<String> {
+    val allAnswers = mutableListOf(result.correct_answer)
+    allAnswers.addAll(result.incorrect_answers)
+
+    allAnswers.shuffle(Random(System.currentTimeMillis()))
+
+    return allAnswers
 }
